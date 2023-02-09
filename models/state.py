1fa36cbb31becaf0cfce955235b308a15eab0943
ESTHEREKANEM <estekanem1@gmail.com>
2023-02-09 06:51:35 -0800

    engine updated

---
+++ models/state.py
@@ -0,0 +1,10 @@
+tate module """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ class State that inherits from BaseModel
+    Attributes:
+        name: string - empty string
+    """
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+"""user class module"""
+
+class User(BaseModel):
+    """Class User from BaseModel
+    Attributes:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
